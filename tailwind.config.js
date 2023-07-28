6acf74930513379bbf5017f063b159dd8486a6c0
seobinbang7 <newmso5382@gmail.com>
2023-07-29 00:45:58 +0900

    🎉tailwind에 colors 추가된 채 커밋

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./client/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#373F67',
+        secondary: '#5A85EE',
+        tertiary: '#719CF7',
+        contents: '#9DA1B4',
+        negative: '#E11900',
+      },
+    },
   },
   plugins: [],
 };
