33adda0768f2a521a22b25f28282d28fc982d4be
Imeanstar <mike4404@naver.com>
2023-07-29 00:01:20 +0900

    ✨ html 상단 바 제외 완성, register js시작

--- client/lib/dom/css.js
+++ client/lib/dom/css.js
@@ -50,13 +50,13 @@ function getCss(node, prop) {
 // jQuery
 
 // 대상에게 원하는 css 속성을 추가 = set
-function setCss(node, prop, value) {
+export function setCss(node, prop, value) {
   if (typeof node === 'string') {
     node = getNode(node);
   }
-  if (!(prop in document.body.style)) {
-    syntaxError('setCSS 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다.');
-  }
+  //if (!(prop in document.body.style)) {
+  //  syntaxError('setCSS 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다.');
+  //}
   if (!value) {
     syntaxError('setCSS 함수의 세 번째 인자는 필수값 입니다.');
   }
